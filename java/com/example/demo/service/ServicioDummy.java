d0a7586bc61ae737bcab5797d515a6563ed21b57
amolerov <amolerov@BOG-4KWDRV2.mshome.net>
2020-02-17 09:00:59 -0500

    temp

---
+++ java/com/example/demo/service/ServicioDummy.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioDummy {
+	
+	public String miMetodoEmpty() {
+		return "Si funciona el servicio";
+	}
+}
